1a8828765119da2ab7fad6952130f3783c78ee86
Petr Gostin <U.238@rambler.ru>
2019-05-26 15:06:34 +0300

    update

--- Application.php
+++ Application.php
@@ -1,1 +1,93 @@
+<?php
+namespace Pandora3\Libs\Application;
 
+use Pandora3\Core\Application\BaseApplication;
+use Pandora3\Core\Container\Container;
+use Pandora3\Core\Interfaces\DatabaseConnectionInterface;
+use Pandora3\Core\Interfaces\SessionInterface;
+use Pandora3\Libs\Database\DatabaseConnection;
+use Pandora3\Libs\Session\Session;
+use Pandora3\Plugins\Authorisation\Authorisation;
+use Pandora3\Plugins\Authorisation\UserProviderInterface;
+
+/**
+ * Class Application
+ * @package Pandora3\Libs\Application
+ *
+ * @property-read string $baseUri
+ * @property-read DatabaseConnectionInterface $database
+ * @property-read Authorisation $auth
+ */
+abstract class Application extends BaseApplication {
+
+	/** @var Application $instance */
+	protected static $instance;
+
+	/**
+	 * @return static
+	 */
+	public static function getInstance(): self {
+		return self::$instance;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function run(string $mode = self::MODE_DEV): void {
+		self::$instance = $this;
+		parent::run($mode);
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getSecret(): string {
+		return $this->config->get('secret');
+	}
+
+	/**
+	 * @internal
+	 * @return string
+	 */
+	protected function getBaseUri(): string {
+		return $this->config->get('baseUri') ?? '/';
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	protected function getConfig(): array {
+		return array_replace(
+			$this->loadConfig($this->path.'/../config/config.php'),
+			$this->loadConfig($this->path.'/../config/config'.ucfirst($this->mode).'.php'),
+			$this->loadConfig($this->path.'/../config/local.php')
+		);
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	protected function dependencies(Container $container): void {
+		parent::dependencies($container);
+
+		$container->setShared(DatabaseConnectionInterface::class, DatabaseConnection::class);
+		$container->setShared(SessionInterface::class, Session::class);
+
+		$this->setProperty('baseUri', function() { return $this->getBaseUri(); });
+
+		if ($this->config->has('database')) {
+			$container->set(DatabaseConnection::class, function() {
+				return new DatabaseConnection($this->config->get('database'));
+			});
+			$this->setProperty('database', DatabaseConnectionInterface::class);
+		}
+
+		// todo: default UserProviderInterface
+		$container->set(UserProviderInterface::class, function() {
+			return null;
+		});
+
+		$this->setProperty('auth', Authorisation::class);
+	}
+
+}
