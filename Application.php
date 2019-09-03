a1fc0891be124acf94a05bb37d7561d5acc0fb90
Petr Gostin <U.238@rambler.ru>
2019-09-03 21:16:29 +0300

    middlewares

--- Application.php
+++ Application.php
@@ -1,11 +1,8 @@
 <?php
 namespace Pandora3\Libs\Application;
 
-use Closure;
 use Pandora3\Core\Application\BaseApplication;
-use Pandora3\Core\Application\Exceptions\UnregisteredMiddlewareException;
 use Pandora3\Core\Container\Container;
-use Pandora3\Core\Controller\Controller;
 use Pandora3\Core\Http\Request;
 use Pandora3\Core\Http\Response;
 use Pandora3\Core\Interfaces\DatabaseConnectionInterface;
@@ -15,12 +12,9 @@ use Pandora3\Core\Interfaces\RequestInterface;
 use Pandora3\Core\Interfaces\ResponseInterface;
 use Pandora3\Core\Interfaces\RouterInterface;
 use Pandora3\Core\Interfaces\SessionInterface;
-use Pandora3\Core\Middleware\Interfaces\MiddlewareInterface;
-use Pandora3\Core\Middleware\MiddlewareChain;
-use Pandora3\Core\Middleware\MiddlewareDispatcher;
+use Pandora3\Core\MiddlewareRouter\MiddlewareRouter;
 use Pandora3\Core\Router\Exceptions\RouteNotFoundException;
 use Pandora3\Core\Router\RequestHandler;
-use Pandora3\Core\Router\Router;
 use Pandora3\Libs\Database\DatabaseConnection;
 use Pandora3\Libs\Session\Session;
 use Pandora3\Plugins\Authorisation\Authorisation;
@@ -41,9 +35,9 @@ abstract class Application extends BaseApplication {
 
 	/** @var Application $instance */
 	protected static $instance;
-
-	/** @var array $middlewares */
-	protected $middlewares = [];
+	
+	/** @var RequestDispatcherInterface $dispatcher */
+	protected $dispatcher;
 
 	/**
 	 * @return static
@@ -61,11 +55,11 @@ abstract class Application extends BaseApplication {
 	}
 
 	/**
-	 * @param string $middleware
+	 * @param string $name
 	 * @param string $className
 	 */
-	protected function registerMiddleware(string $middleware, string $className): void {
-		$this->middlewares[$middleware] = $className;
+	protected function registerMiddleware(string $name, string $className): void {
+		$this->container->set("middleware.$name", $className);
 	}
 
 	/**
@@ -109,45 +103,11 @@ abstract class Application extends BaseApplication {
 	}
 	
 	/**
-	 * @param string $name
-	 * @return MiddlewareInterface|null
-	 * @throws UnregisteredMiddlewareException
+	 * @param RouterInterface|MiddlewareRouter $router
+	 * @param array $routes
 	 */
-	public function getMiddleware(string $name): ?MiddlewareInterface {
-		if (!array_key_exists($name, $this->middlewares)) {
-			throw new UnregisteredMiddlewareException($name);
-		}
-		return $this->container->get($this->middlewares[$name]);
-	}
-	
-	/**
-	 * @param RequestHandlerInterface|RequestDispatcherInterface $handler
-	 * @param MiddlewareInterface[] $middlewares
-	 * @return RequestHandlerInterface|RequestDispatcherInterface
-	 */
-	public function chainMiddlewares($handler, ...$middlewares) {
-		$middlewares = array_map( function(string $middleware) {
-			try {
-				return $this->getMiddleware($middleware);
-			} catch (UnregisteredMiddlewareException $ex) {
-				throw $ex;
-			}
-		}, $middlewares);
-		$chain = new MiddlewareChain(...$middlewares);
-		
-		if ($handler instanceof RequestDispatcherInterface) {
-			return new MiddlewareDispatcher($handler, $chain);
-		} else {
-			return $chain->wrapHandler($handler);
-		}
-	}
-	
-	/**
-	 * @param array $arguments
-	 * @return RequestHandlerInterface
-	 */
-	protected function dispatch(array &$arguments): RequestHandlerInterface {
-		foreach($this->getRoutes() as $routePath => $handler) {
+	protected function registerRoutes($router, array $routes) {
+		foreach($routes as $routePath => $handler) {
 			$middlewares = [];
 			if (is_array($handler)) {
 				[$middlewares, $handler] = $handler;
@@ -156,29 +116,36 @@ abstract class Application extends BaseApplication {
 				}
 			}
 			
-			if ($handler instanceof Closure) {
-				$handler = new RequestHandler($handler);
-			} else if (is_string($handler)) {
-				if (!array_intersect(
-					[RequestHandlerInterface::class, RequestDispatcherInterface::class],
-					class_implements($handler)
-				)) {
-					throw new \LogicException("Route handler for '$routePath' must be [Closure] or implement [RequestHandlerInterface] or [RequestDispatcherInterface]");
-				}
-				$handler = $this->container->get($handler);
-				if ($handler instanceof Controller) {
-					$handler->setApplication($this);
-				}
+			if ($router instanceof MiddlewareRouter) {
+				$router->add($routePath, $handler, $middlewares);
+			} else {
+				$router->add($routePath, $handler);
 			}
-			
-			if ($middlewares) {
-				$handler = $this->chainMiddlewares($handler, ...$middlewares);
-			}
-			$this->router->add($routePath, $handler);
 		}
-		
+	}
+	
+	/**
+	 * @return RequestDispatcherInterface
+	 */
+	protected function getDispatcher(): RequestDispatcherInterface {
+		if (is_null($this->dispatcher)) {
+			/** @var RouterInterface|MiddlewareRouter $dispatcher */
+			$dispatcher = $this->container->get(RouterInterface::class);
+			$routes = $this->getRoutes();
+			$this->registerRoutes($dispatcher, $routes);
+			$this->dispatcher = $dispatcher;
+		}
+		return $this->dispatcher;
+	}
+	
+	/**
+	 * @param array $arguments
+	 * @return RequestHandlerInterface
+	 */
+	protected function dispatch(array &$arguments): RequestHandlerInterface {
+		$dispatcher = $this->getDispatcher();
 		try {
-			return $this->router->dispatch($this->request->uri, $arguments);
+			return $dispatcher->dispatch($this->request->uri, $arguments);
 		} catch (RouteNotFoundException $ex) {
 			return new RequestHandler( function(RequestInterface $request) {
 				return $this->page404($request);
@@ -212,8 +179,11 @@ abstract class Application extends BaseApplication {
 
 		$container->setDependencies([
 			RequestInterface::class => Request::class,
-			RouterInterface::class => Router::class,
+			RouterInterface::class => MiddlewareRouter::class,
 		]);
+		$container->set(MiddlewareRouter::class, function(Container $c, $routes = []) {
+			return new MiddlewareRouter($routes, $c);
+		});
 		$container->set(Request::class, function() {
 			$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 			$uri = (strncmp($uri, '/', 1) === 0 ? '' : '/').$uri;
@@ -221,8 +191,7 @@ abstract class Application extends BaseApplication {
 		});
 
 		$this->setProperties([
-			'request' => RequestInterface::class,
-			'router' => RouterInterface::class,
+			'request' => RequestInterface::class
 		]);
 
 		$container->setDependenciesShared([
@@ -244,12 +213,14 @@ abstract class Application extends BaseApplication {
 			return null;
 		});
 
-		$container->set(AuthorisedMiddleware::class, function() {
-			return new AuthorisedMiddleware($this->getUnauthorisedHandler(), $this->container->get(Authorisation::class));
+
+		$this->setProperty('auth', Authorisation::class);
+
+		$container->set(AuthorisedMiddleware::class, function(Container $c) {
+			return new AuthorisedMiddleware($this->getUnauthorisedHandler(), $c->get('auth'));
 		});
 
 		$this->registerMiddleware('auth', AuthorisedMiddleware::class);
-		$this->setProperty('auth', Authorisation::class);
 	}
 	
 	protected function execute(): void {
